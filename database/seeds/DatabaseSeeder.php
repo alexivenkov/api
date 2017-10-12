353d7da192dbcad0e4270360d37c206d5967bb10
alex <Aleksandr.Ivenkov@noveogroup.com>
2017-10-12 13:05:58 +0300

    implement users seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        \App\Models\User::truncate();
+
+        $this->call(UsersTableSeeder::class);
     }
 }
